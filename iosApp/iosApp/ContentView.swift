a3bf273a94642238617da21dcb41b3b871beb4cd
Patricia Dumontier <pdumonti@cylinea.com>
2025-04-13 06:29:45 -0600

    iOS app runs and displays Pizza Order form

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,7 +4,7 @@ import ComposeApp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+        return IosEntryPointKt.startComposeApp()
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
