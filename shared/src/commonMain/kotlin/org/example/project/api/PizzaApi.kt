355c79064f020f1f4f9c92918c43747308ace313
Patricia Dumontier <pdumonti@cylinea.com>
2025-04-13 08:01:38 -0600

    Fix sendPizzaOrder not working

--- shared/src/commonMain/kotlin/org/example/project/api/PizzaApi.kt
+++ shared/src/commonMain/kotlin/org/example/project/api/PizzaApi.kt
@@ -3,8 +3,9 @@ package org.example.project.api
 import io.ktor.client.HttpClient
 import io.ktor.client.request.*
 import io.ktor.client.request.setBody
-import io.ktor.client.statement.*
 import io.ktor.client.call.*
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import org.example.project.models.OrderResponse
 import org.example.project.models.PizzaOrder
 
@@ -17,8 +18,8 @@ expect fun createHttpClient(): HttpClient
  */
 suspend fun sendPizzaOrder(order: PizzaOrder): OrderResponse {
     val client = createHttpClient()
-    val response: HttpResponse = client.post("http://localhost:8080/order") {
+    return client.post("http://localhost:8080/order") {
+        contentType(ContentType.Application.Json)
         setBody(order)
-    }
-    return response.body()
+    }.body()
 }
